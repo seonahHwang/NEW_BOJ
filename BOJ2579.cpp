46680dc2e29b7ccf19ae51b9f19f7baac5bc3ea3
brightest-ko <37548372+brightest-ko@users.noreply.github.com>
2018-05-29 16:20:46 +0900

    Update BOJ2579.cpp

--- BOJ2579.cpp
+++ BOJ2579.cpp
@@ -30,8 +30,8 @@ int main(){
         scanf("%d", &arr[i]);
     }
     dp[0]=arr[0];
-    dp[1]=max(arr[0],arr[1]); // 이렇게 고쳐야 할 것 같고.. 틀리다고 나오나요?아님 디버깅 문제인가요?
-    dp[2]=max(arr[0]+arr[2],arr[1]+arr[2]);
+    dp[1]=max(arr[0],arr[1]+arr[0]); // 이렇게 고쳐야 할 것 같고.. 틀리다고 나오나요?아님 디버깅 문제인가요?
+    dp[2]=max(dp[0]+arr[2],arr[1]+arr[2]);
     for(int i=3;i<n;i++){
         dp[i]=max(dp[i-3]+arr[i-1]+arr[i-2],dp[i-2]+arr[i]);
     }
