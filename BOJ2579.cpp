f00181aab2c3bca3c56e46f8e5ab01505746f6aa
hwang <37287788+hwang11@users.noreply.github.com>
2018-05-29 12:42:34 +0900

    Update BOJ2579.cpp

--- BOJ2579.cpp
+++ BOJ2579.cpp
@@ -30,7 +30,7 @@ int main(){
         scanf("%d", &arr[i]);
     }
     dp[0]=arr[0];
-    dp[1]=max(arr[0]+arr[1],arr[1]); -> 이렇게 고쳐도 틀
+    dp[1]=max(arr[0]+arr[1],arr[1]); -> 이렇게 고쳐도 틀리데요 
     dp[2]=max(arr[0]+arr[2],arr[1]+arr[2]);
     for(int i=3;i<n;i++){
         dp[i]=max(dp[i-3]+arr[i-1]+arr[i-2],dp[i-2]+arr[i]);
