e8d51ef9842aab1b75cde06c1575ecc4f8866fe1
lovehero0206 <tjsdk2769@gmail.com>
2018-05-30 18:31:22 +0900

    BOJ 10844 쉬운 계단 수

---
+++ BOJ/BOJ/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BOJ
+//
+//  Created by stayes on 2018. 5. 30..
+//  Copyright © 2018년 BOJ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
